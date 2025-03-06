6f76726521a5cbcaf4ce97ff196c2e687bddb3d6
Aswarth <212g1a0403@gmail.com>
2025-03-06 21:59:07 +0530

    map and forEach

--- Transforming and Filtering with map() and forEach().js
+++ Transforming and Filtering with map() and forEach().js
@@ -1,1 +1,15 @@
-console.log('map')
+function processProducts(dict){
+    let ans = dict.map((ele)=>(ele.name))
+    console.log(ans)
+    dict.forEach((ele)=>{
+        if(ele.price>50){
+            console.log(`${ele.name} is above $${ele.price}`)
+        }
+        else{
+            console.log(`${ele.name} is below $${ele.price}`)
+        }
+    })
+}
+Input = [{ name: "Laptop", price: 1000 }, { name: "Mouse", price: 20 }]
+processProducts(Input)
+// processProducts()
